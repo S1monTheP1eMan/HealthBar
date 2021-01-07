115a5cd685183e705a133a17e2afb9aff708eba0
S1monTheP1eMan <70568227+S1monTheP1eMan@users.noreply.github.com>
2021-01-07 12:51:04 +0200

    Init

---
+++ Assets/Scripts/TextChanger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TextChanger : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _textMesh;
+
+    public void ChangeText(float value)
+    {
+        _textMesh.text = Convert.ToString(value);
+    }
+}
